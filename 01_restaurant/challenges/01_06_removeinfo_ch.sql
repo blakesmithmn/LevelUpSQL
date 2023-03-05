80792e5c202bae8bd55b257a6896a0f61d3d124a
Blake Smith <blake.smith.mn@gmail.com>
2023-03-05 21:05:24 +0000

    wrote a query to deal with cancellations for a customers reservation - using an inner join

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,15 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+SELECT FirstName, CUSTOMERS.CustomerID, ReservationID, Date, PartySize FROM CUSTOMERS
+  INNER JOIN RESERVATIONS
+    ON CUSTOMERS.CustomerID = RESERVATIONS.CustomerID
+  WHERE FirstName = 'Norby'
+    AND DATE > '2022-07-24';
+
+DELETE FROM RESERVATIONS WHERE RESERVATION.ID = 2000;
+
+-- OR
+
+UPDATE RESERVATIONS SET DATE = NULL WHERE RESERVATIONID = 2000;
