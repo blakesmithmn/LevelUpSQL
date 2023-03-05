c9db8249f32ce49e747ac8dc9ca886493ce4a06d
Blake Smith <blake.smith.mn@gmail.com>
2023-03-05 20:42:54 +0000

    created multiple menu options using queries on the dishes table ... end of challenge 3

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -3,3 +3,14 @@
 -- Create a report with all the items sorted by price (lowest to highest).
 -- Create a report showing appetizers and beverages.
 -- Create a report with all items except beverages.
+
+SELECT Type, Name, Description, Price FROM DISHES
+  ORDER BY PRICE;
+
+SELECT Name, Description, Price, Type FROM DISHES
+  WHERE TYPE IN ('Appetizer', 'Beverage')
+  ORDER BY PRICE;
+
+SELECT Name, Description, Price, Type FROM Dishes
+  WHERE TYPE != 'Beverage'
+  ORDER BY PRICE;
